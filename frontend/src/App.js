56ac88c2992ddac28d63c1d3686b1d111bd7a394
yohannes-taye <yohannestaye83@gmail.com>
2025-09-12 19:25:02 +0800

    Add front end

---
+++ frontend/src/App.js
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+
+function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch("/users")
+      .then((res) => res.json())
+      .then((data) => setUsers(data))
+      .catch((err) => console.error(err));
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>Users</h1>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            {user.name} ({user.email})
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
