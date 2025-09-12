4fdc8d9ea6b06f057a5a8e72990455c9eae9b072
yohannes-taye <yohannestaye83@gmail.com>
2025-09-12 18:50:10 +0800

    Inital commit

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const { Pool } = require("pg");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: "localhost",
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASS,
+  port: 5432,
+});
+
+app.get("/", (req, res) => res.send("API is running"));
+
+app.get("/users", async (req, res) => {
+  const result = await pool.query("SELECT * FROM users");
+  res.json(result.rows);
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
